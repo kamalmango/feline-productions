36b29296c1753cf20199ff0b48c4fd808e089775
Kamal Mango <kamal.mango7@gmail.com>
2022-05-13 17:54:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import felineLogo from './feline-logo.svg';
 import './App.css';
 
 function App() {
+  const [password, setPassword] = useState(true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <img src={felineLogo} className="logo" alt="logo" />
+      <div className='password-container'>
+        <div>
+        <input type='password' onClick={() => setPassword(true)} className='password' placeholder='Password' />
+        {!password && <p className='error-message'>The password you've entered is incorrect. Please try again</p>}
+        </div>
+        <button onClick={() => setPassword(false)} className="button">Submit</button>
+      </div>
     </div>
   );
 }
